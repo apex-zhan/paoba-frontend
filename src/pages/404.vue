2bac6368c641a6f62f8c8fca73695e6ff3a8ea61
thiszxw <3327332153@qq.com>
2024-11-02 02:55:11 +0800

    feat:优化UI和功能

---
+++ src/pages/404.vue
@@ -0,0 +1,30 @@
+<!-- src/views/NotFound.vue -->
+<template>
+  <div class="not-found">
+    <van-empty description="页面不存在">
+      <template #bottom>
+        <van-button type="primary" @click="goHome">返回首页</van-button>
+      </template>
+    </van-empty>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const goHome = () => {
+  router.push("/");
+};
+</script>
+
+<style scoped>
+.not-found {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f7f8fa;
+}
+</style>
